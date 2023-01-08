046005f2eeb7c1dc57c9f8aafbc7efbbf7a3f3a0
Alex Fife <alextfife@gmail.com>
2023-01-08 12:05:06 -0500

    Added email and VIN
    also working on Legal page.
    
    Other changes
    + switched from arbituary title labels on pages at the top to Nav bars
    + Changed formatting for profile
    
    Issues
    - incomplete skeleton
    - nav bars have buttons but they arent visible yet
    
    Thoughts:
    • profile page could have the type of car you have with a nice picture in addition to the VIN

---
+++ TesMuffler/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TesMuffler
+//
+//  Created by Alex Fife on 11/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }}
